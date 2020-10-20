40da54c988915b08d729917bc723818b1e479fca
Izzet Can Karakus <izzetcankarakus@gmail.com>
2020-10-20 20:30:31 +0300

    cleanup

--- docker-scripts/copy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if ! output=$(docker inspect gitlab-conf 2>/dev/null); then
-      echo "No container gitlab-conf found."
-
-
-else
-          echo "container found"
-fi
--- jenkins-test/test.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -it -v 545621b3a2da0992bccceb54c453918976f904766735d65a9929b5ed47da2976:/from -v jenkins-test_gitlab-log:/to alpine ash -c "rm -rf /to;cd /from; cp -av . /to"
-if [ $? == 1 ]; then
-    echo  "hekki"
-fi
